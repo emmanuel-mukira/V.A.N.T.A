52d9b462fd1b83c289f3c0637120f3bc4fc9e147
Emmanuel <emmanuel.mukira@student.moringaschool.com>
2023-05-12 11:25:40 +0300

    first commit

--- vanta/src/components/contact.js
+++ vanta/src/components/contact.js
@@ -140,10 +140,8 @@ export const Contact = (props) => {
       <div id="footer">
         <div className="container text-center">
           <p>
-            &copy; 2023 Issaaf Kattan React Land Page Template. Design by{" "}
-            <a href="http://www.templatewire.com" rel="nofollow">
-              TemplateWire
-            </a>
+            &copy; 2023 Emmanuel Mukira . 
+            
           </p>
         </div>
       </div>
